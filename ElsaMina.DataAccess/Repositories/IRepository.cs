5f31570c6bb86b64c2dffc8c35c04c464addd16f
slimf1 <slimane.f41@gmail.com>
2023-05-27 17:50:46 +0200

    Rework repository classes

--- ElsaMina.DataAccess/Repositories/IRepository.cs
+++ ElsaMina.DataAccess/Repositories/IRepository.cs
@@ -1,6 +1,8 @@
+using ElsaMina.DataAccess.Models;
+
 namespace ElsaMina.DataAccess.Repositories;
 
-public interface IRepository<T, in TKey> : IDisposable
+public interface IRepository<T, in TKey> : IDisposable where T : IKeyed<TKey>
 {
     Task<T> GetByIdAsync(TKey key);
     Task<IEnumerable<T>> GetAllAsync();
