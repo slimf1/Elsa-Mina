b4c93b48481f38ba6863de91a3b5cb29123f88d2
slimf1 <slimane.f41@gmail.com>
2023-05-27 17:39:13 +0200

    Fix unit tests

--- ElsaMina.Test/Core/Services/Rooms/RoomsManagerTest.cs
+++ ElsaMina.Test/Core/Services/Rooms/RoomsManagerTest.cs
@@ -14,7 +14,7 @@ public class RoomsManagerTest
 {
     private ILogger _logger;
     private IConfigurationManager _configurationManager;
-    private IRoomParametersRepository _roomParametersRepository;
+    private IRepository<RoomParameters, string> _roomParametersRepository;
 
     private RoomsManager _roomsManager;
 
@@ -23,7 +23,7 @@ public class RoomsManagerTest
     {
         _logger = Substitute.For<ILogger>();
         _configurationManager = Substitute.For<IConfigurationManager>();
-        _roomParametersRepository = Substitute.For<IRoomParametersRepository>();
+        _roomParametersRepository = Substitute.For<IRepository<RoomParameters, string>>();
 
         _roomsManager = new RoomsManager(_logger, _configurationManager, _roomParametersRepository);
     }
