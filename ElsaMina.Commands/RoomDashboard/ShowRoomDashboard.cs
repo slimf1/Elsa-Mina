ee6fdb156812b613e9470419298f415edb0c44b6
slimf1 <slimane.f41@gmail.com>
2023-05-29 13:12:18 +0200

    Room dashboard styling

--- ElsaMina.Commands/RoomDashboard/ShowRoomDashboard.cs
+++ ElsaMina.Commands/RoomDashboard/ShowRoomDashboard.cs
@@ -13,15 +13,6 @@ namespace ElsaMina.Commands.RoomDashboard;
 
 public class ShowRoomDashboard : ICommand
 {
-    private const string TABLE_STYLE = "border-collapse: collapse ; width: 100%";
-
-    private const string FIRST_ROW_STYLE =
-        "border: 1px solid #68a ; text-align: center ; padding: 0px ; height: 1px ; text-align: center ; background-color: rgba(102 , 136 , 170 , 0.45)";
-
-    private const string CELL_STYLE = "border: 1px solid #68a ; text-align: center ; padding: 4px";
-
-    private const string ROW_STYLE = "border: 1px solid #68a ; text-align: center ; padding: 4px";
-
     public static string Name => "room-dashboard";
     public bool IsPrivateMessageOnly => true;
     public bool IsWhitelistOnly => true; // todo : seul un mec authed sur la room peut
@@ -67,15 +58,13 @@ public class ShowRoomDashboard : ICommand
             context.SendHtmlPage("dashboard-error", "<p>Could not find room parameters somehow</p>");
             return;
         }
+        
         if (context.IsPm)
         {
             context.Locale = new CultureInfo(roomParameters.Locale
                                              ?? _configurationManager.Configuration.DefaultLocaleCode);
         }
-        //var languages = _resourcesService.SupportedLocales.Select(culture => 
-        //    new CultureInfoDrop(culture, culture.Name == context.Locale.Name));
-        var languages = _resourcesService.SupportedLocales;
-        
+
         var template = await _templatesManager.GetTemplate("room-dashboard", new Dictionary<string, object>
         {
             ["room_name"] = room.Name,
@@ -83,86 +72,9 @@ public class ShowRoomDashboard : ICommand
             ["room_parameters"] = roomParameters,
             ["bot_name"] = _configurationManager.Configuration.Name,
             ["trigger"] = _configurationManager.Configuration.Trigger,
-            ["languages"] = languages,
-            ["selected_language"] = context.Locale.Name
+            ["languages"] = _resourcesService.SupportedLocales
         });
+        
         context.SendHtmlPage($"{roomId}dashboard", template.RemoveNewlines());
-    }
-
-    private async Task<string> GetPanel(IContext context, string roomId)
-    {
-        var roomParameters = await _roomParametersRepository.GetByIdAsync(roomId);
-        if (roomParameters == null)
-        {
-            return "<p>Could not find room parameters somehow</p>";
-        }
-
-        if (context.IsPm)
-        {
-            context.Locale =
-                new CultureInfo(roomParameters.Locale ?? _configurationManager.Configuration.DefaultLocaleCode);
-        }
-        var botName = _configurationManager.Configuration.Name;
-        var localesOptions = _resourcesService.SupportedLocales.Select(culture => $"""
-            <option
-                value="{culture.Name}"
-                {(context.Locale.Name == culture.Name ? "selected" : "")}>
-                {culture.NativeName.Capitalize()}
-            </option>
-        """);
-
-        return $$"""
-            <form
-                style="padding: 2rem;"
-                id="locale"
-                data-submitsend="/w {{botName}},-rc {{roomId}},{locale},{errors},{autocorrect}">
-                <table style="{{TABLE_STYLE}}">
-                    <thead>
-                        <tr style="{{FIRST_ROW_STYLE}}">
-                            <th colspan="3"><h1>{{context.GetString("room_dashboard", roomId)}}</h1></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr style="{{ROW_STYLE}}">
-                            <td style="{{CELL_STYLE}}">{{context.GetString("dashboard_locale_name")}}</td>
-                            <td style="{{CELL_STYLE}}">
-                                <select name="locale">
-                                    {{string.Join("", localesOptions)}}
-                                </select>
-                            </td>
-                            <td style="{{CELL_STYLE}}">{{context.GetString("dashboard_locale_description")}}</td>
-                        </tr>
-                        <tr style="{{ROW_STYLE}}">
-                            <td style="{{CELL_STYLE}}">{{context.GetString("dashboard_errors_name")}}</td>
-                            <td style="{{CELL_STYLE}}">
-                                <input
-                                    type="checkbox"
-                                    id="errors"
-                                    name="errors"
-                                    {{(roomParameters.IsShowingErrorMessages ?? false ? "checked" : "")}} />
-                            </td>
-                            <td style="{{CELL_STYLE}}">{{context.GetString("dashboard_errors_description")}}</td>
-                        </tr>
-                        <tr style="{{ROW_STYLE}}">
-                            <td style="{{CELL_STYLE}}">{{context.GetString("dashboard_autocorrect_name")}}</td>
-                            <td style="{{CELL_STYLE}}">
-                                <input
-                                    type="checkbox"
-                                    id="autocorrect"
-                                    name="autocorrect"
-                                    {{(roomParameters.IsCommandAutocorrectEnabled ?? false ? "checked" : "")}} />
-                            </td>
-                            <td style="{{CELL_STYLE}}">{{context.GetString("dashboard_autocorrect_description")}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <br /> <br />
-                <button
-                    class="button"
-                    type="submit">
-                    {{context.GetString("submit")}}
-                </button>
-            </form>
-        """;
     }
 }
