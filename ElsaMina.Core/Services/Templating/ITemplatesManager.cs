d3552d253cb44e1ada9e61165b0aeff71bc8b818
slimf1 <slimane.f41@gmail.com>
2023-06-06 20:14:49 +0200

    get template

--- ElsaMina.Core/Services/Templating/ITemplatesManager.cs
+++ ElsaMina.Core/Services/Templating/ITemplatesManager.cs
@@ -1,6 +1,11 @@
+using ElsaMina.Core.Templates;
+
 namespace ElsaMina.Core.Services.Templating;
 
 public interface ITemplatesManager
 {
     Task<string> GetTemplate(string templateName, object model);
+    Task<string> GetTemplate<TPage, TViewModel>()
+        where TPage : LocalizableTemplatePage<LocalizableViewModel>
+        where TViewModel : LocalizableViewModel;
 }
--- ElsaMina.Core/Services/Templating/TemplatesManager.cs
+++ ElsaMina.Core/Services/Templating/TemplatesManager.cs
@@ -1,3 +1,5 @@
+using ElsaMina.Core.Services.DependencyInjection;
+using ElsaMina.Core.Templates;
 using RazorLight;
 
 namespace ElsaMina.Core.Services.Templating;
@@ -11,8 +13,24 @@ public class TemplatesManager : ITemplatesManager
         .UseMemoryCachingProvider()
         .Build());
 
+    private readonly IDependencyContainerService _dependencyContainerService;
+
+    public TemplatesManager(IDependencyContainerService dependencyContainerService)
+    {
+        _dependencyContainerService = dependencyContainerService;
+    }
+
     public async Task<string> GetTemplate(string templateName, object model)
     {
         return await _razorLightEngine.Value.CompileRenderAsync(templateName, model);
+    }
+
+    public async Task<string> GetTemplate<TPage, TViewModel>()
+        where TPage : LocalizableTemplatePage<LocalizableViewModel>
+        where TViewModel : LocalizableViewModel
+    {
+        var template = _dependencyContainerService.Resolve<TPage>();
+        var viewModel = _dependencyContainerService.Resolve<TViewModel>();
+        return await _razorLightEngine.Value.RenderTemplateAsync(template, viewModel);
     }
 }
