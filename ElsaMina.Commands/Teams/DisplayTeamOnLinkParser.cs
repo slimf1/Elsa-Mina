8adbf7baffb3dfcc632be4470c982318eb049f46
slimf1 <slimane.f41@gmail.com>
2023-06-11 12:02:10 +0200

    Team preview for links

--- ElsaMina.Commands/Teams/DisplayTeamOnLinkParser.cs
+++ ElsaMina.Commands/Teams/DisplayTeamOnLinkParser.cs
@@ -3,6 +3,8 @@ using ElsaMina.Core.Commands.Parsers;
 using ElsaMina.Core.Contexts;
 using ElsaMina.Core.Services.Clock;
 using ElsaMina.Core.Services.DependencyInjection;
+using ElsaMina.Core.Services.Templating;
+using ElsaMina.Core.Templates.TeamPreview;
 using ElsaMina.Core.Utils;
 using Serilog;
 
@@ -11,22 +13,25 @@ namespace ElsaMina.Commands.Teams;
 public class DisplayTeamOnLinkParser : ChatMessageParser
 {
     private static readonly Regex TEAM_LINK_REGEX = new(@"(https:\/\/pokepast\.es\/[0-9A-Fa-f]{16})|(https:\/\/www\.coupcritique\.fr\/entity\/teams\/\d+)");
-    private const int USER_DELAY = 20;
+    private const int USER_DELAY = 30;
     
     private readonly Dictionary<string, DateTimeOffset> _lastTeamTimes = new();
 
     private readonly ILogger _logger;
     private readonly IClockService _clockService;
     private readonly ITeamProviderFactory _teamProviderFactory;
+    private readonly ITemplatesManager _templatesManager;
     
     public DisplayTeamOnLinkParser(ILogger logger,
         IDependencyContainerService dependencyContainerService,
         IClockService clockService,
-        ITeamProviderFactory teamProviderFactory)
+        ITeamProviderFactory teamProviderFactory,
+        ITemplatesManager templatesManager)
         : base(dependencyContainerService)
     {
         _clockService = clockService;
         _teamProviderFactory = teamProviderFactory;
+        _templatesManager = templatesManager;
         _logger = logger;
     }
 
@@ -39,7 +44,7 @@ public class DisplayTeamOnLinkParser : ChatMessageParser
 
         var now = _clockService.CurrentDateTimeOffset;
         if (_lastTeamTimes.TryGetValue(context.Sender.UserId, out var userLastTeamTime)
-            && (now - userLastTeamTime).TotalSeconds < USER_DELAY)
+            && (now - userLastTeamTime).TotalSeconds < USER_DELAY && !context.IsSenderWhitelisted)
         {
             return;
         }
@@ -62,7 +67,15 @@ public class DisplayTeamOnLinkParser : ChatMessageParser
             return;
         }
         var sets = ShowdownTeams.DeserializeTeamExport(sharedTeam.TeamExport);
+
+        var template = await _templatesManager.GetTemplate("TeamPreview/TeamPreview", new TeamPreviewViewModel
+        {
+            Author = sharedTeam.Author,
+            Culture = context.Locale,
+            Sender = context.Sender.Name,
+            Team = sets
+        });
         
-        context.Reply( string.Join(", ", sets.Select(t => t.Species)));
+        context.SendHtml(template.RemoveNewlines());
     }
 }
