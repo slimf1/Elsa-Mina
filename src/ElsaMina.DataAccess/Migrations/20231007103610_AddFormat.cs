7ea329c525c0bd9533b2060a9e6df1e6cf932562
slimf1 <slimane.f41@gmail.com>
2023-10-07 12:36:38 +0200

    Add format migration

---
+++ src/ElsaMina.DataAccess/Migrations/20231007103610_AddFormat.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElsaMina.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFormat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Format",
+                table: "Teams",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Format",
+                table: "Teams");
+        }
+    }
+}
