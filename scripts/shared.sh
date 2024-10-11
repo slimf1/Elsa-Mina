d5b51a733d3d71caac5c4d31aba7cc1dd790552b
slimf1 <slimane.fakani.dev@gmail.com>
2024-10-11 22:01:55 +0200

    lancement en nohup

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,5 +1,5 @@
 ﻿#!/bin/bash
 
-BUILD_PROPERTIES="-p:DebugType=None -p:DebugSymbols=false --self-contained false"
+BUILD_PROPERTIES="-p:DebugType=None -p:DebugSymbols=false"
 CONFIGURATION="Release"
 RUNTIME_ID="linux-x64"
