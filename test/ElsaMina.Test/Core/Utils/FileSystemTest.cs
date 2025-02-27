012458f95a8e4a67b78693bd67d6f3961c24ab0b
slimf1 <slimane.fakani.dev@gmail.com>
2025-02-28 00:05:28 +0100

    tests: added unit tests for file system helpers

---
+++ test/ElsaMina.Test/Core/Utils/FileSystemTest.cs
@@ -0,0 +1,32 @@
+using ElsaMina.Core.Utils;
+
+namespace ElsaMina.Test.Core.Utils;
+
+public class FileSystemTest
+{
+    [Test]
+    public void Test_MakeRelativePath_ShouldReturnRelativePath()
+    {
+        // Arrange
+        const string absolutePath = "/Library/Stuff/Lol/Test/Stuff.png";
+
+        // Act
+        var result = FileSystem.MakeRelativePath(absolutePath, referencePath: "/Library/Stuff/Lol");
+        
+        // Assert
+        Assert.That(result, Is.EqualTo("Lol/Test/Stuff.png"));
+    }
+    
+    [Test]
+    public void Test_RemoveExtension_ShouldRemoveExtension()
+    {
+        // Arrange
+        const string fileName = "Stuff.png";
+
+        // Act
+        var result = fileName.RemoveExtension();
+        
+        // Assert
+        Assert.That(result, Is.EqualTo("Stuff"));
+    }
+}
