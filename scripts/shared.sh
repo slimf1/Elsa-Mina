ef84965af45c1ea94ba38e9f18c1f62d35043657
slimf1 <slimane.fakani.dev@gmail.com>
2025-03-05 23:48:15 +0100

    Revert "ci: try build single file"
    This reverts commit 3a0fe740c796a151946c6922925a5186a6edd14c.

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export BUILD_PROPERTIES="-p:DebugType=None -p:DebugSymbols=false -p:SelfContained=true -p:PublishSingleFile=true"
+export BUILD_PROPERTIES="-p:DebugType=None -p:DebugSymbols=false"
 export CONFIGURATION=Release
 export RUNTIME_ID=linux-x64
