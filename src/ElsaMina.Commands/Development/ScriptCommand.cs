8a319b11d14dd485cc328c951fe236e0428ee6de
slimf1 <slimane.fakani.dev@gmail.com>
2025-12-12 19:22:07 +0100

    Revert "feat: remove script command"
    This reverts commit 1e58a258ee3c51df6ec5f9f7b3d8d09d65fe9fdc.

---
+++ src/ElsaMina.Commands/Development/ScriptCommand.cs
@@ -0,0 +1,51 @@
+using ElsaMina.Core;
+using ElsaMina.Core.Commands;
+using ElsaMina.Core.Contexts;
+using ElsaMina.Core.Services.DependencyInjection;
+using ElsaMina.Logging;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ElsaMina.Commands.Development;
+
+[NamedCommand("script")]
+public class ScriptCommand : DevelopmentCommand
+{
+    private readonly IDependencyContainerService _dependencyContainerService;
+
+    public ScriptCommand(IDependencyContainerService dependencyContainerService)
+    {
+        _dependencyContainerService = dependencyContainerService;
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public class Globals
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public IContext Context { get; set; }
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public IDependencyContainerService Container { get; set; }
+    }
+
+    public override async Task RunAsync(IContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var options = ScriptOptions.Default.WithReferences(typeof(Bot).Assembly)
+                .WithReferences(typeof(ScriptCommand).Assembly)
+                .WithImports("ElsaMina.Core");
+            var globals = new Globals { Context = context, Container = _dependencyContainerService };
+            var result = await CSharpScript.EvaluateAsync(context.Target, options, globals: globals,
+                cancellationToken: cancellationToken);
+            if (result != null)
+            {
+                context.Reply(result.ToString());
+            }
+        }
+        catch (Exception exception)
+        {
+            Log.Error(exception, "An error occurred while using script command.");
+            context.Reply($"An error occurred: {exception.Message}");
+        }
+    }
+}
