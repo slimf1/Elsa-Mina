3bd20a9fcb9b8199834a4c0a221a58d46c936984
slimf1 <slimane.fakani.dev@gmail.com>
2026-02-17 21:10:02 +0100

    build: remove runtime id and run w/ runtime

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ./scripts/shared.sh
-dotnet build ./src/ElsaMina.Console/ElsaMina.Console.csproj ${BUILD_PROPERTIES} -c "${CONFIGURATION}" -r "${RUNTIME_ID}" --no-restore
+dotnet build ./src/ElsaMina.Console/ElsaMina.Console.csproj ${BUILD_PROPERTIES} -c "${CONFIGURATION}" --no-restore
--- scripts/shared.sh
+++ scripts/shared.sh
@@ -2,4 +2,3 @@
 
 export BUILD_PROPERTIES="-p:DebugType=None -p:DebugSymbols=false"
 export CONFIGURATION=Release
-export RUNTIME_ID=linux-x64
