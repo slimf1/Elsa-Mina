3823e8bd260a3bfac285e34031c63c7a1ad428df
slimf1 <slimane.fakani.dev@gmail.com>
2026-03-18 21:27:22 +0100

    fix(http): update HttpClient settings for experimental image requests

--- src/ElsaMina.Core/Services/Http/HttpService.cs
+++ src/ElsaMina.Core/Services/Http/HttpService.cs
@@ -1,21 +1,44 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace ElsaMina.Core.Services.Http;
 
 public class HttpService : IHttpService
 {
+    private readonly SocketsHttpHandler _handler = new()
+    {
+        AutomaticDecompression = DecompressionMethods.All,
+        PooledConnectionLifetime = TimeSpan.FromMinutes(2),
+        ConnectCallback = async (context, cancellationToken) =>
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+            {
+                NoDelay = true
+            };
+            await socket.ConnectAsync(context.DnsEndPoint.Host, context.DnsEndPoint.Port, cancellationToken);
+            return new NetworkStream(socket, ownsSocket: true);
+        }
+    };
+
     private readonly HttpClient _httpClient;
 
     public HttpService()
     {
-        _httpClient = new HttpClient
+        _httpClient = new HttpClient(_handler)
         {
             Timeout = TimeSpan.FromSeconds(30),
-            // User agent
-        }; 
-        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", 
+        };
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "image/avif,image/webp,image/apng,*/*;q=0.8");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US,en;q=0.9");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Referer", "https://imgur.com/");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-dest", "image");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-mode", "no-cors");
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-site", "cross-site");
     }
 
     public async Task<IHttpResponse<TResponse>> PostJsonAsync<TRequest, TResponse>(
