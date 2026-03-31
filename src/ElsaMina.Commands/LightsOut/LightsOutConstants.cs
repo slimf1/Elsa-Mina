58c8a715dc15f0a957508c1987a73866edac7504
Distrib-PS <theodelhay@orange.fr>
2026-03-31 20:22:18 +0200

    feat(games): add levels 11-15 to Lights Out with 8x8 grid
    - Level 11: 7x7, 12 presses
    - Level 12-15: 8x8 grid, 10 to 22 presses
    - Levels 1-10 unchanged
    - Cell size adapts to 30px for 8x8 grids

--- src/ElsaMina.Commands/LightsOut/LightsOutConstants.cs
+++ src/ElsaMina.Commands/LightsOut/LightsOutConstants.cs
@@ -2,22 +2,27 @@ namespace ElsaMina.Commands.LightsOut;
 
 public static class LightsOutConstants
 {
-    public const int MAX_LEVEL = 10;
+    public const int MAX_LEVEL = 15;
     public static readonly TimeSpan INACTIVITY_TIMEOUT = TimeSpan.FromMinutes(2);
 
     // (GridSize, Presses) per level
     public static readonly (int GridSize, int Presses)[] LEVEL_CONFIGURATIONS =
     [
-        (5, 3),  // Level 1
-        (5, 4),  // Level 2
-        (5, 5),  // Level 3
-        (5, 6),  // Level 4
-        (5, 7),  // Level 5
-        (6, 5),  // Level 6
-        (6, 7),  // Level 7
-        (6, 9),  // Level 8
-        (7, 7),  // Level 9
-        (7, 10)  // Level 10
+        (5, 3),   // Level 1
+        (5, 4),   // Level 2
+        (5, 5),   // Level 3
+        (5, 6),   // Level 4
+        (5, 7),   // Level 5
+        (6, 5),   // Level 6
+        (6, 7),   // Level 7
+        (6, 9),   // Level 8
+        (7, 7),   // Level 9
+        (7, 10),  // Level 10
+        (7, 12),  // Level 11
+        (8, 10),  // Level 12
+        (8, 14),  // Level 13
+        (8, 18),  // Level 14
+        (8, 22)   // Level 15
     ];
 
     public static (int GridSize, int Presses) GetLevelConfig(int level)
