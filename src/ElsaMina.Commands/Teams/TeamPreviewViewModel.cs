18f86c070147d2fefb0a79f58bc9f0ce05bb1ce9
slimf1 <slimane.fakani.dev@gmail.com>
2026-04-02 22:12:45 +0200

    refactor(teams): use IReadOnlyList for Team property in TeamPreviewViewModel

--- src/ElsaMina.Commands/Teams/TeamPreviewViewModel.cs
+++ src/ElsaMina.Commands/Teams/TeamPreviewViewModel.cs
@@ -4,7 +4,7 @@ namespace ElsaMina.Commands.Teams;
 
 public class TeamPreviewViewModel : LocalizableViewModel
 {
-    public IEnumerable<PokemonSet> Team { get; init; }
+    public IReadOnlyList<PokemonSet> Team { get; init; }
     public string Sender { get; init; }
     public string Author { get; init; }
 }
